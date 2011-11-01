94c4a318a22c986a61faad4bde03a7e0cc73579d
Alex Frenkel <sirshurf@yahoo.com>
2011-11-01 03:05:24 -0700

    Added Backup support

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -1,92 +1,104 @@
 <?php
-class Menu_IndexController extends Zend_Controller_Action {
-    
-    public function indexAction(){
-        
+class Menu_IndexController extends Zend_Controller_Action
+{
+
+	public function indexAction ()
+	{
 		$objMenuTable = new Menu_Model_Db_Menu();
-		$objMenuSelect = $objMenuTable->select (TRUE);
-		
-		$arrOptions = array ("hiddengrid" => false, "caption" => "" );
-		
-		$grid = new Ingot_JQuery_JqGrid ( 'menu', new Ingot_JQuery_JqGrid_Adapter_DbTableSelect ( $objMenuSelect ),$arrOptions );
-		$grid->setIdCol ( Menu_Model_Db_Menu::COL_ID_MENU );
-		$grid->setLocalEdit();		
-				
-		$grid->addColumn ( new Ingot_JQuery_JqGrid_Column ( Menu_Model_Db_Menu::COL_CODE, array('editable' => true) ) );
-		$grid->addColumn ( new Ingot_JQuery_JqGrid_Column ( Menu_Model_Db_Menu::COL_LABEL, array('editable' => true) ) );
-				
-		Ingot_JQuery_JqGrid_Column_DoubleColumn::createSelectColumn($grid, 'Parent', array(), FALSE );
-				
-		$grid->addColumn ( new Ingot_JQuery_JqGrid_Column ( Menu_Model_Db_Menu::COL_URI, array('editable' => true) ) );
-		$grid->addColumn ( new Ingot_JQuery_JqGrid_Column ( Menu_Model_Db_Menu::COL_MODULE, array('editable' => true) ) );
-		$grid->addColumn ( new Ingot_JQuery_JqGrid_Column ( Menu_Model_Db_Menu::COL_CONTROLLER , array('editable' => true)) );
-		$grid->addColumn ( new Ingot_JQuery_JqGrid_Column ( Menu_Model_Db_Menu::COL_ACTION, array('editable' => true) ) );
-		$grid->addColumn ( new Ingot_JQuery_JqGrid_Column ( Menu_Model_Db_Menu::COL_CSS, array('editable' => true) ) );
-		
-		
-		Ingot_JQuery_JqGrid_Column_DoubleColumn::createSelectColumn($grid, 'Resources', array(), FALSE );
-		
-		//$grid->addColumn ( new Ingot_JQuery_JqGrid_Column ( Menu_Model_Db_Menu::COL_RESOURCE ) );
-		$grid->addColumn ( new Ingot_JQuery_JqGrid_Column ( Menu_Model_Db_Menu::COL_PRIVELEGE, array('editable' => true) ) );	
-		$grid->addColumn ( new Ingot_JQuery_JqGrid_Column ( Menu_Model_Db_Menu::COL_ORDER, array('editable' => true) ) );	
-				
-		$objPlugin = $grid->getPager ();
-		$objPlugin->setDefaultAdd ();
-		$objPlugin->setDefaultEdit ();
-		$objPlugin->setDefaultDel ();
+		$objMenuSelect = $objMenuTable->select(TRUE);
+		$arrOptions = array("hiddengrid" => false, "caption" => "");
+		$grid = new Ingot_JQuery_JqGrid('menu', new Ingot_JQuery_JqGrid_Adapter_DbTableSelect($objMenuSelect), $arrOptions);
+		$grid->setIdCol(Menu_Model_Db_Menu::COL_ID_MENU);
+		$grid->setLocalEdit();
+		$grid->addColumn(new Ingot_JQuery_JqGrid_Column(Menu_Model_Db_Menu::COL_CODE, array('editable' => true)));
+		$grid->addColumn(new Ingot_JQuery_JqGrid_Column(Menu_Model_Db_Menu::COL_LABEL, array('editable' => true)));
+		Ingot_JQuery_JqGrid_Column_DoubleColumn::createSelectColumn($grid, 'Parent', array(), FALSE);
+		$grid->addColumn(new Ingot_JQuery_JqGrid_Column(Menu_Model_Db_Menu::COL_URI, array('editable' => true)));
+		$grid->addColumn(new Ingot_JQuery_JqGrid_Column(Menu_Model_Db_Menu::COL_MODULE, array('editable' => true)));
+		$grid->addColumn(new Ingot_JQuery_JqGrid_Column(Menu_Model_Db_Menu::COL_CONTROLLER, array('editable' => true)));
+		$grid->addColumn(new Ingot_JQuery_JqGrid_Column(Menu_Model_Db_Menu::COL_ACTION, array('editable' => true)));
+		$grid->addColumn(new Ingot_JQuery_JqGrid_Column(Menu_Model_Db_Menu::COL_CSS, array('editable' => true)));
+		Ingot_JQuery_JqGrid_Column_DoubleColumn::createSelectColumn($grid, 'Resources', array(), FALSE);
+		$grid->addColumn(new Ingot_JQuery_JqGrid_Column(Menu_Model_Db_Menu::COL_PRIVELEGE, array('editable' => true)));
+		$grid->addColumn(new Ingot_JQuery_JqGrid_Column(Menu_Model_Db_Menu::COL_ORDER, array('editable' => true)));
+		$objPlugin = $grid->getPager();
+		$objPlugin->setDefaultAdd();
+		$objPlugin->setDefaultEdit();
+		$objPlugin->setDefaultDel();
 		$grid->setDblClkEdit();
+		$grid->registerPlugin(new Ingot_JQuery_JqGrid_Plugin_ToolbarFilter());
+		$this->view->grid = $grid->render();
 		
-		$grid->registerPlugin ( new Ingot_JQuery_JqGrid_Plugin_ToolbarFilter () );
-		$this->view->grid = $grid->render ();
-    }
-    
-    
-    public function editmenuAction(){
-                
-        $strOper = $this->_request->getParam ( 'oper' );
+		$objForm = new Menu_Form_UploadMenu();
+		$this->view->objForm = $objForm;
 		
-		$objMenuTable = new Labadmin_Models_Menu();
-		$objRow = null;
-		$arrStatus  =array();
-		switch ($strOper) {
-			case "edit" :
-				$intId = ( int ) $this->_request->getParam ( 'id' );
-				$objSelect = $objMenuTable->select (TRUE);
-				$objSelect->where ( Labadmin_Models_Menu::COL_ID_MENU." = ?", $intId );
-				$objRow = $objMenuTable->fetchRow ( $objSelect );
-			case "add" :
-				if (empty ( $objRow )) {
-					$objRow = $objMenuTable->createRow ();
-				}
-				$arrParams = $this->_request->getParams ();
-				$objRow->setFromArray($arrParams);
-				if( $objRow->save ()){
-				    $arrStatus = array('code' => 'ok', 'msg' => '');
-				} else {
-				    $arrStatus = array('code' => 'error', 'msg' => $this->view->translate('LBL_UPDATE_FAIL') );
-				}
+		$arrActions = array();
+		$arrActions[] = array('module' => 'menu', 'controller' => 'index', "action" => "savebackup", "name" => 'LBL_SAVE_BKP_FILE');
+		$arrActions[] = array('module' => 'menu', 'controller' => 'index', "action" => "loadbackup", "name" => 'LBL_LOAD_BKP_FILE', 'onClick' => '$("#uploader").dialog("open")');
+		$this->view->arrActions = $arrActions;
+	}
+
+	public function savebackupAction ()
+	{
+		Zend_Controller_Action_HelperBroker::getStaticHelper('viewRenderer')->setNoRender();
+		Zend_Controller_Action_HelperBroker::getStaticHelper('layout')->disableLayout();
+		$objMenuTable = new Menu_Model_Db_Menu();
+		$objMenuSelect = $objMenuTable->select(TRUE);
+		$objConfig = new Zend_Config($objMenuTable->fetchAll($objMenuSelect)->toArray());
+		$writer = new Zend_Config_Writer_Array(array('config' => $objConfig, 'filename' => APPLICATION_PATH . '/configs/backup/menu.php'));
+		try {
+			$writer->write();
+			Labels_Model_SystemLabels::setJgrowlMessage('LBL_MENU_SAVE_OK');
+		} catch (Exception $objEx) {
+			Labels_Model_SystemLabels::setJgrowlMessage('LBL_MENU_SAVE_FAILED');
+		}
+		$strUrl = $this->view->url(array('module' => 'menu', 'controller' => 'index', 'action' => 'index'), null, true);
+		$this->_redirect($strUrl);
+	}
+
+	public function loadbackupAction ()
+	{
+		Zend_Controller_Action_HelperBroker::getStaticHelper('viewRenderer')->setNoRender();
+		Zend_Controller_Action_HelperBroker::getStaticHelper('layout')->disableLayout();
+		
+		$form = new Menu_Form_UploadMenu();
+		
+		if ($this->_request->isPost()) {
+			$formData = $this->_request->getPost();
+			if ($form->isValid($formData)) {
 				
-				break;
-			case "del" :				
-				$intId = ( int ) $this->_request->getParam ( 'id' );
-				$objSelect = $objMenuTable->select (TRUE);
-				$objSelect->where ( Labadmin_Models_Menu::COL_ID_MENU." = ?", $intId );
-				$objRow = $objMenuTable->fetchRow ( $objSelect );
-				if (! empty ( $objRow )) {
-					if ($objRow->delete ()){					    
-				        $arrStatus = array('code' => 'ok', 'msg' => '');
-					} else {
-				    $arrStatus = array('code' => 'error', 'msg' => $this->view->translate('LBL_DEL_FAIL') );
+				// success - do something with the uploaded file
+				if ($form->menuFile->isUploaded()) {
+					// foo file given... do something
+					$fullFilePath = $form->getValues();
+					$fullFilePath = $form->menuFile->getFileName();
+					Zend_Debug::dump($fullFilePath);
+					
+					$objConfig = new Zend_Config(require $fullFilePath);
+					
+					Zend_Debug::dump($objConfig->toArray());
+					$objMenuTable = new Menu_Model_Db_Menu();
+					
+					foreach ($objConfig as $objConfigMenuRow) {
+						$objMenuRowSet = $objMenuTable->find($objConfigMenuRow->{Menu_Model_Db_Menu::COL_ID_MENU});
+						
+						if ($objMenuRowSet->count() > 0) {
+							$objMenuRow = $objMenuRowSet->current();
+						} else {
+							// Add New
+							$objMenuRow = $objMenuTable->createRow();
+						}
+						$objMenuRow->setFromArray($objConfigMenuRow->toArray());
+						$objMenuRow->save();
 					}
 				}
-				break;
-		
+			
+			} else {
+				$form->populate($formData);
+			}
 		}
-		
-		Labadmin_Models_Menu::getMenu(TRUE);
-		
-		$this->view->arrStatus = $arrStatus;
-        
-    }
-    
+	
+		$strUrl = $this->view->url(array('module' => 'menu', 'controller' => 'index', 'action' => 'index'), null, true);
+		$this->_redirect($strUrl);
+	}
 }
